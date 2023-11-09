769abbba2cd4a4e0e25cf1e7ec133682e7577d43
Cassie Miller <miller@berkeley.edu>
2023-11-09 11:42:56 -0500

    Fix dockerfile in frontend to expose port 80 to the outside world

--- infrastructure/userdata.sh
+++ infrastructure/userdata.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
 apt update
-apt install -y nodejs npm python3-pip
+apt install -y docker.io 
+apt install -y ruby-full
+apt install -y wget
+
+cd /home/ubuntu
+
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+
+service codedeploy-agent start
