93d83052bc50cc34bc669ec57823036ce628ca55
Cassandra Miller <miller@berkeley.edu>
2023-11-09 15:43:39 -0500

    Dev (#10)
    * Testing signed commit
    
    * remove frontend
    
    * Revert "remove frontend"
    
    This reverts commit 7bf70795141b96ae92e6ef5d9449e368103e9f80.
    
    * remove worthless github actions
    
    * attempt to de-submodule front end again
    
    * Revert "remove frontend"
    
    This reverts commit 7bf70795141b96ae92e6ef5d9449e368103e9f80.
    
    * attempt to de-submodule frontend, take 2
    
    * add frontend
    
    * Add buildspec.yml
    
    * code cleanup and updates to docker-compose.yml
    
    * Tweaks to dockerfiles
    
    * Update docker-image.yml
    
    * Update and rename docker-image.yml to build-docker-images.yml
    
    * Update build-docker-images.yml
    
    * Fix dockerfile in frontend to expose port 80 to the outside world
    
    * Fix dockerfile in frontend to expose port 80 to the outside world
    
    * Begin work on codedeploy code

--- infrastructure/userdata.sh
+++ infrastructure/userdata.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
 apt update
-apt install -y nodejs npm python3-pip
+apt install -y docker.io 
+apt install -y ruby-full
+apt install -y wget
+
+cd /home/ubuntu
+
+# Install docker compose
+curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install and start codedeploy-agent
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
+service codedeploy-agent start
